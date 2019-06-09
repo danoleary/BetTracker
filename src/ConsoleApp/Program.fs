86669fc5632cbe8aeec2410e07a5634fa89031cb
Dan O'Leary <danieloleary89@gmail.com>
2019-06-09 23:20:48 +0100

    download data from google sheets

--- src/ConsoleApp/Program.fs
+++ src/ConsoleApp/Program.fs
@@ -20,6 +20,13 @@ let includedBookies = [
     Guid.Parse("1478766f-8480-46a7-8134-d35a3d64017f") //comeon
     Guid.Parse("ac133ab6-99b8-4690-880d-50b092f12c78") //betvictor need to add in cash out event
     Guid.Parse("904c9199-8b81-4a96-af9e-c7d83317ab0c") //boylesports
+    Guid.Parse("56f01b6b-7545-463a-bbe0-224963b844ae") //betuk
+    Guid.Parse("5987a4a8-8797-4f08-bb6d-20290f26ab6d") //888sport
+    Guid.Parse("ce7042e9-9b50-4690-a1f9-8b079c7430d1") //laba360
+    Guid.Parse("41c661ad-1dd5-4730-9ab1-63fd1427e22b") //7 best bets
+    Guid.Parse("41ad0a7c-0bfb-41ae-aa3c-42d50433f3cb") // 21bet
+    Guid.Parse("234f675e-5314-4d5b-a78c-318ed7e64839") //the pools
+    Guid.Parse("eda560dc-de49-4593-a616-2cebc48c95e9") //bwin
 ]
 
 let printState (desc:string) =
@@ -51,7 +58,7 @@ let main argv =
 
     let addBookieCommands =
         Bookies
-            .Load("/Users/danieloleary/Documents/Github/BetTracker/src/ConsoleApp/data/bookies.csv")
+            .Load("https://docs.google.com/spreadsheets/d/13AXqyemjgob_DoqN84jUxnrJDHqcBtuGvHAchjR5BMI/export?exportFormat=csv")
             .Rows
         |> Seq.map (
             fun x -> {  AggregateId = AggregateId x.Id;
@@ -62,7 +69,7 @@ let main argv =
                             
     let depositCommands =
         Deposits
-            .Load("/Users/danieloleary/Documents/Github/BetTracker/src/ConsoleApp/data/deposits.csv")
+            .Load("https://docs.google.com/spreadsheets/d/1vK_8HtQHRDSpoMQmapPQY6kg3JHVfFDCoNLFNg6Ck8s/export?exportFormat=csv")
             .Rows
         |> Seq.map (fun x -> {
                                 AggregateId = AggregateId x.Id;
@@ -73,7 +80,7 @@ let main argv =
 
     let withdrawalCommands =
         Withdrawals
-            .Load("/Users/danieloleary/Documents/Github/BetTracker/src/ConsoleApp/data/withdrawals.csv")
+            .Load("https://docs.google.com/spreadsheets/d/1ofH2vj_uaiwcDG_26DFyln5RwkXwTgjSJJVsLUfrbNg/export?exportFormat=csv")
             .Rows
         |> Seq.map (fun x -> { 
                                 AggregateId = AggregateId x.Id;
@@ -84,7 +91,7 @@ let main argv =
 
     let placeBackBetCommands =
         PlaceBackBets
-            .Load("/Users/danieloleary/Documents/Github/BetTracker/src/ConsoleApp/data/placebackbets.csv")
+            .Load("https://docs.google.com/spreadsheets/d/1xhHtOQNb1mm-r-UWFHxdiK4tewjXPrLKPz6P1dvXcfE/export?exportFormat=csv")
             .Rows
         |> Seq.map (fun x -> {
                                 AggregateId = AggregateId x.BookieId;
@@ -97,7 +104,7 @@ let main argv =
 
     let settleBackBetCommands =
         SettleBackBets
-            .Load("/Users/danieloleary/Documents/Github/BetTracker/src/ConsoleApp/data/settlebackbets.csv")
+            .Load("https://docs.google.com/spreadsheets/d/1TlZ6aPSJRyW_5-YSbNp9KQ_kEarFUrue9t9HtPbFdDY/export?exportFormat=csv")
             .Rows
         |> Seq.map (fun x -> {
                                 AggregateId = AggregateId x.BookieId;
@@ -109,7 +116,7 @@ let main argv =
 
     let placeFreeBetCommands =
         PlaceFreeBets
-            .Load("/Users/danieloleary/Documents/Github/BetTracker/src/ConsoleApp/data/placefreebets.csv")
+            .Load("https://docs.google.com/spreadsheets/d/1StGsS86PuitnzfgCxipWUIIOJU2kr5Aq6uwL6eJXOPk/export?exportFormat=csv")
             .Rows
         |> Seq.map (fun x -> {
                                 AggregateId = AggregateId x.BookieId;
@@ -122,7 +129,7 @@ let main argv =
 
     let settleFreeBetCommands =
         SettleBackBets
-            .Load("/Users/danieloleary/Documents/Github/BetTracker/src/ConsoleApp/data/settlefreebets.csv")
+            .Load("https://docs.google.com/spreadsheets/d/1aM7Kz2EFm0Iy3f_k-tP79xW-AyC136kHDT3qir54XCo/export?exportFormat=csv")
             .Rows
         |> Seq.map (fun x -> {
                                 AggregateId = AggregateId x.BookieId;
@@ -134,7 +141,7 @@ let main argv =
 
     let cashoutbackbets =
         CashOutBackBets
-            .Load("/Users/danieloleary/Documents/Github/BetTracker/src/ConsoleApp/data/cashoutbackbets.csv")
+            .Load("https://docs.google.com/spreadsheets/d/1yJGP-7Rx2yrV1FGz7zzWOZK6AScCOqUtyVHELKpQ0mE/export?exportFormat=csv")
             .Rows
         |> Seq.map (fun x -> 
                                 {
